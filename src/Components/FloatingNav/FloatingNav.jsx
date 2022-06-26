2b468a7ed29570999d5b295a0fa06ebec8cf9f54
atharvaarankalle <atharvanz@gmail.com>
2022-06-26 19:17:49 +1200

    Add floating navbar component

--- src/Components/FloatingNav/FloatingNav.jsx
+++ src/Components/FloatingNav/FloatingNav.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './FloatingNav.css';
+import {AiOutlineHome, AiOutlineUser, AiOutlineBook, AiOutlinePhone} from 'react-icons/ai';
+import { useState } from 'react';
 
 const FloatingNav = () => {
+  const [activeNav, setActiveNav] = useState('#');
   return (
-    <div>FloatingNav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#About" onClick={() => setActiveNav('#About')} className={activeNav === '#About' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#Experience" onClick={() => setActiveNav('#Experience')} className={activeNav === '#Experience' ? 'active' : ''}><AiOutlineBook /></a>
+      <a href="#Contact" onClick={() => setActiveNav('#Contact')} className={activeNav === '#Contact' ? 'active' : ''}><AiOutlinePhone /></a>
+    </nav>
   )
 }
 
